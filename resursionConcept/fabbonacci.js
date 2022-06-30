eb831a51ad8aa55259779e9fd37f8cf77657fa2a
Mohd Ziya <iamziya53@gmail.com>
2022-06-30 16:20:19 -0400

    Practice

--- resursionConcept/fabbonacci.js
+++ resursionConcept/fabbonacci.js
@@ -1,8 +1,9 @@
 function fibo(first,second,counter){
-    if (counter<10){
+    if (counter<=10){
         console.log(first);
         let c=first+second;
         return fibo(second,c,counter+1)
     }
+    
 }
 fibo(1,1,1)
